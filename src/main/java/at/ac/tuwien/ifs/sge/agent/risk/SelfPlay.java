b70e57a6b420b586fb85df6eb09c9b775b9992e9
DESKTOP-B4LNKON\stefan <stefan.steinheber@gmail.com>
2024-01-09 09:22:30 +0100

    print

--- src/main/java/at/ac/tuwien/ifs/sge/agent/risk/SelfPlay.java
+++ src/main/java/at/ac/tuwien/ifs/sge/agent/risk/SelfPlay.java
@@ -2,12 +2,17 @@ package at.ac.tuwien.ifs.sge.agent.risk;
 
 import at.ac.tuwien.ifs.sge.agent.risk.util.DatasetWriter;
 import at.ac.tuwien.ifs.sge.agent.risk.util.RiskHasher;
+import at.ac.tuwien.ifs.sge.agent.risk.util.Tuple;
 import at.ac.tuwien.ifs.sge.engine.Logger;
 import at.ac.tuwien.ifs.sge.game.risk.board.Risk;
 import at.ac.tuwien.ifs.sge.game.risk.board.RiskAction;
+import org.joda.time.format.DateTimeFormat;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class SelfPlay {
@@ -24,22 +29,28 @@ public class SelfPlay {
     public void run() {
         risk = new Risk();
 
+        List<Tuple<String, Integer>> record = new ArrayList<>();
+        int iterations = 0;
         while (!risk.isGameOver()) {
             RiskAction action = null;
             if (risk.getCurrentPlayer() == 0) {
-                action = player1.computeNextAction(risk, 30000, TimeUnit.MILLISECONDS);
+                action = player1.computeNextAction(risk, 10000, TimeUnit.MILLISECONDS);
             } else if (risk.getCurrentPlayer() == 1) {
-                action = player2.computeNextAction(risk, 30000, TimeUnit.MILLISECONDS);
+                action = player2.computeNextAction(risk, 10000, TimeUnit.MILLISECONDS);
             } else {
                 risk.doAction();
                 continue;
             }
+            System.out.println(DateTimeFormat.mediumDateTime().print(new Date().getTime()) + ": iteration done");
             Risk newState = (Risk) risk.doAction(action);
             String state_str = RiskHasher.CSV.encodeGame(risk, 0);
             String actionEncoding = RiskHasher.CSV.encodeAction(action, risk);
-            DatasetWriter.CSV.getInstance().appendToCSV(String.join(",", state_str, actionEncoding), .0f);
+            record.add(new Tuple<>(String.join(",", state_str, actionEncoding), iterations));
             risk = newState;
+            iterations++;
         }
+
+        record.forEach(t -> DatasetWriter.CSV.getInstance().appendToCSV(t.getA(), t.getB(), (float) risk.getUtilityValue(0)));
 
         System.out.println("Utility 1: " + risk.getUtilityValue(0));
         System.out.println("Utility 2: " + risk.getUtilityValue(1));
