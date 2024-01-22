732d43737b746fb0ee9261488c7251f6e4cd040d
Brrrrz <80272953+Brrrrz@users.noreply.github.com>
2024-01-22 07:29:07 +0100

    Added file with Thompson and UCB1 combination

---
+++ src/main/java/at/ac/tuwien/ifs/sge/agent/risk/montecarlo/selection/ThompsonSamplingWithUCB1Strategy.java
@@ -0,0 +1,53 @@
+package at.ac.tuwien.ifs.sge.agent.risk.montecarlo.selection;
+
+import at.ac.tuwien.ifs.sge.agent.risk.montecarlo.MCTSNode;
+import at.ac.tuwien.ifs.sge.agent.risk.montecarlo.MCTSTree;
+import at.ac.tuwien.ifs.sge.game.risk.board.Risk;
+import at.ac.tuwien.ifs.sge.game.risk.board.RiskAction;
+
+import java.util.List;
+import java.util.Set;
+import org.apache.commons.math3.distribution.BetaDistribution;
+
+public class ThompsonSamplingWithUCB1Strategy extends MCTSSelectionStrategy<Risk, RiskAction> {
+
+    @Override
+    public MCTSNode<Risk, RiskAction> select(MCTSNode<Risk, RiskAction> root, MCTSTree<Risk, RiskAction> tree) {
+        Risk risk = root.getState();
+        double maxThompsonSample = Double.NEGATIVE_INFINITY;
+        MCTSNode<Risk, RiskAction> selected = null;
+
+        if (root.isLeaf())
+            return root;
+        List<MCTSNode<Risk,RiskAction>> nodesForState = tree.getAllNodesForState(risk, tree);
+        if (nodesForState == null || nodesForState.isEmpty()) {
+            UCB1SelectionStrategy<Risk, RiskAction> ucb1 = new UCB1SelectionStrategy<>();
+            return ucb1.select(root, tree);
+        } else {
+            for (MCTSNode<Risk, RiskAction> node : nodesForState) {
+                double sample = sampleBeta(node.getIntProperty("successes", 0), node.getIntProperty("failures", 0));
+                if (sample > maxThompsonSample) {
+                    maxThompsonSample = sample;
+                    selected = node;
+                }
+            }
+        }
+
+        return selected;
+    }
+
+    /**
+     * BetaDistribution is used to sample from a beta distribution with alpha and beta
+     *
+     * @param alpha alpha
+     * @param beta beta
+     * @return sample
+     */
+    public double sampleBeta(int alpha, int beta) {
+        double sample = 0.0;
+        if (alpha > 0 || beta > 0) {
+            sample = new BetaDistribution(alpha, beta).sample();
+        }
+        return sample;
+    }
+}
